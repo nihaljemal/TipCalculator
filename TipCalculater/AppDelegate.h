2714a07991dc23fd75af0e315143201f108e2bcb
Nihal Riyadh Jemal <nihalj@fb.com>
2018-06-26 13:32:59 -0700

    Initial Commit

---
+++ TipCalculater/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculater
+//
+//  Created by Nihal Riyadh Jemal on 6/26/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TipCalculater/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculater
+//
+//  Created by Nihal Riyadh Jemal on 6/26/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
